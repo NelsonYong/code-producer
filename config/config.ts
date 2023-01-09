c531de87af2e7b8fc877bff074eb984ca36f7836
YongGit <1013588891@qq.com>
2023-01-09 13:27:06 +0800

    feat: mpa

--- config/config.ts
+++ config/config.ts
@@ -1,20 +1,58 @@
 import { defineConfig } from 'umi'
-
-export default defineConfig({
+const { NODE_ENV } = process.env
+const config = defineConfig({
 	npmClient: 'pnpm',
 	tailwindcss: {},
-	base: '/code-producer/site/',
+	base: NODE_ENV === 'production' ? '/code-producer/site/' : '',
 	publicPath: '/code-producer/site/',
 	plugins: ['@umijs/plugins/dist/tailwindcss'],
 	outputPath: 'site',
 	history: {
 		type: 'browser',
 	},
-	mpa: {
-		entry: {
-			json2ts: 'Json2ProTable/index.tsx',
-			json2protable: 'Json2Ts/index.tsx',
-			guide: 'Guide/index.tsx',
-		},
+
+	define: {
+		NODE_ENV,
 	},
 })
+
+export default NODE_ENV === 'production'
+	? Object.assign(
+			{},
+			{
+				...config,
+				mpa: {
+					entry: {
+						json2ts: 'Json2ProTable/index.tsx',
+						json2protable: 'Json2Ts/index.tsx',
+						guide: 'Guide/index.tsx',
+					},
+				},
+			}
+	  )
+	: Object.assign(
+			{},
+			{
+				...config,
+				routes: [
+					{
+						path: '/guide',
+						name: '首页',
+						icon: 'home',
+						component: './Guide',
+					},
+					{
+						path: '/json2ts',
+						name: 'json2ts',
+						icon: 'home',
+						component: './Json2Ts',
+					},
+					{
+						path: '/json2protable',
+						name: 'json2protable',
+						icon: 'home',
+						component: './Json2ProTable',
+					},
+				],
+			}
+	  )
---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+const index = () => {
+	return <div>123</div>
+}
+
+export default index
